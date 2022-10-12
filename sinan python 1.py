10c6195a928cc73886c5ee75b5c6bcb8c827f660
sinansakeer <115392311+sinansakeer@users.noreply.github.com>
2022-10-12 10:10:56 +0530

    Add files via upload

---
+++ sinan python 1.py
@@ -0,0 +1,11 @@
+name1="anyname1"
+name2="anyname2"
+name3="anyname3"
+name4="anyname4"
+name5="anyname5"
+
+print(name1,name2,name3,name4,name5)
+
+namecontainer=["anyname6","anyname7","anyname8","anyname9","anyname10"]
+
+print(namecontainer[0],namecontainer[1],namecontainer[2],namecontainer[3],namecontainer[4])
